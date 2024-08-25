e90fcd6021dd0253ddbdb2ed55e86666959c4a6f
TARAKA RAMA RAO <tarakaramarao0506@gmail.com>
2024-08-25 19:17:31 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+const counters = document.querySelectorAll('.counter');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const counter = entry.target;
+            const target = +counter.getAttribute('data-target');
+            const updateCounter = () => {
+                const count = +counter.innerText;
+                const increment = target / 200;
+
+                if (count < target) {
+                    counter.innerText = Math.ceil(count + increment);
+                    setTimeout(updateCounter, 20);
+                } else {
+                    counter.innerText = target;
+                }
+            };
+
+            updateCounter();
+            observer.unobserve(counter);
+        }
+    });
+}, { threshold: 0.1 });
+
+counters.forEach(counter => {
+    observer.observe(counter);
+});
+
